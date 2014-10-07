e320ef8f161394ae5d5884a0decf2011c5a34a44
Tim Hui <timboh56@gmail.com>
2014-10-07 03:30:08 -0700

    gitignore and resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,4 @@
+ENV["REDISTOGO_URL"] ||= "redis://redistogo:f6a484c2e3c498ccdb4ed94357d89e6f@greeneye.redistogo.com:9548/"
+
 uri = URI.parse(ENV["REDISTOGO_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
