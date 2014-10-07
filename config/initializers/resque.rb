2aba51850101995170f4c4b4e99e33bb927738a1
Tim Hui <timboh56@gmail.com>
2014-10-07 02:15:15 -0700

    woops

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,2 @@
-ENV["REDISTOGO_URL"] ||= "redis://redistogo:f6a484c2e3c498ccdb4ed94357d89e6f@greeneye.redistogo.com:9548/"
-
 uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host =&gt; uri.host, :port =&gt; uri.port, :password =&gt; uri.password)
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
