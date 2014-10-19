a083ded1f7d32a7545428b738bc6259a4bf0ed2d
Tim Hui <timboh56@gmail.com>
2014-10-19 01:41:08 -0700

    removed unnecessaries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,127 @@
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/extras/dataTables.responsive
+var window = window || document.window;
+
+function set_bounce() {
+	setInterval(function () {
+		setTimeout( function () {
+				bounce();
+			}, 2000);
+	}, 2000);
+}
+
+function bounce() {
+	$(".bounce").animate({
+		height: "+=10px"
+	}, 1000);
+	$(".bounce").animate({
+		height: "-=10px"
+	}, 1000);
+}
+
+function getElementFontColor(elm) {
+	var fc	= window.getComputedStyle(elm).color;
+		fc	= fc.match(/\((.*)\)/)[1];
+		fc	= fc.split(",");
+	for (var i = 0; i < fc.length; i++) {
+		fc[i] = parseInt(fc[i], 10);
+	}
+	return fc;
+}
+
+function generateRGB() {
+	var color = [];
+	for (var i = 0; i < 3; i++) {
+		var num = Math.floor(Math.random()*225);
+		while (num < 25) {
+			num = Math.floor(Math.random()*225);
+		}
+		color.push(num);
+	}
+	return color;
+}
+
+function rgb2hex(color) {
+	var hex = [];
+	for (var i = 0; i < 3; i++) {
+		hex.push(color[i].toString(16));
+		if (hex[i].length < 2) { hex[i] = "0" + hex[i]; }
+	}
+	return "#" + hex[0] + hex[1] + hex[2];
+}
+
+function calculateDistance(current, next) {
+	var distance = [];
+	for (var i = 0; i < 3; i++) {
+		distance.push(Math.abs(current[i] - next[i]));
+	}
+	return distance;
+}
+
+var incrementStops = 50;
+function calculateIncrement(distance) {
+	var increment = [];
+	for (var i = 0; i < 3; i++) {
+		increment.push(Math.abs(Math.floor(distance[i] / incrementStops)));
+		if (increment[i] == 0) {
+			increment[i]++;
+		}
+	}
+	return increment;
+}
+
+var iteration = Math.round(1000 / (incrementStops/2));
+function createTransition(id) {
+	var elm				= document.getElementById(id);
+	var currentColor	= getElementFontColor(elm);
+	var randomColor		= generateRGB();
+	var distance		= calculateDistance(currentColor, randomColor);
+	var increment		= calculateIncrement(distance);
+	
+	function transition() {
+		
+		if (currentColor[0] > randomColor[0]) {
+			currentColor[0] -= increment[0];
+			if (currentColor[0] <= randomColor[0]) {
+				increment[0] = 0;
+			}
+		} else {
+			currentColor[0] += increment[0];
+			if (currentColor[0] >= randomColor[0]) {
+				increment[0] = 0;
+			}
+		}
+		
+		if (currentColor[1] > randomColor[1]) {
+			currentColor[1] -= increment[1];
+			if (currentColor[1] <= randomColor[1]) {
+				increment[1] = 0;
+			}
+		} else {
+			currentColor[1] += increment[1];
+			if (currentColor[1] >= randomColor[1]) {
+				increment[1] = 0;
+			}
+		}
+		
+		if (currentColor[2] > randomColor[2]) {
+			currentColor[2] -= increment[2];
+			if (currentColor[2] <= randomColor[2]) {
+				increment[2] = 0;
+			}
+		} else {
+			currentColor[2] += increment[2];
+			if (currentColor[2] >= randomColor[2]) {
+				increment[2] = 0;
+			}
+		}
+		elm.style.color = rgb2hex(currentColor);
+		
+		if (increment[0] == 0 && increment[1] == 0 && increment[2] == 0) {
+			clearInterval(handler);
+			createTransition(id);
+		}
+	}
+	var handler = setInterval(transition, iteration);
+}
