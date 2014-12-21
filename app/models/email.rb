7fd0a0c82f976aa0f3e2360b2c2fdbb60f2c8599
Tim Hui <timboh56@gmail.com>
2014-12-21 13:29:18 -0800

    added gmails csv

--- app/models/email.rb
+++ app/models/email.rb
@@ -7,4 +7,19 @@ class Email
 	belongs_to :craig_cram
 
 	scope :unused, lambda { where(used: false) }
+
+
+  def self.open_emails_csv
+    p "Opening proxies csv"
+
+    Dir["emails/*.csv"].each do |csv_file_path|
+
+      p csv_file_path
+      CSV.foreach(csv_file_path) do |row|
+        email = row[5]
+        password = row[6]
+        Email.create!(email: email, password: password) unless Email.where(email: email, password: password).exists?
+      end
+  	end
+  end
 end
