c95e65d6e959e68f08000df457a975f0ec10c1eb
Tim Hui <timboh56@gmail.com>
2014-12-29 19:46:49 -0800

    added phone field to person

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,6 +3,7 @@ class Person
 
 	field :name, type: String
 	field :email, type: String
+	field :phone, type: String
 	field :google_id, type: String
 	field :available, type: Boolean, default: true
 	field :clearbit
