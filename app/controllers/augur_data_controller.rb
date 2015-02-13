19e9406c276d6b27474e83651848b13cac7a1672
Tim Hui <timboh56@gmail.com>
2015-02-12 22:33:25 -0800

    added puts

--- app/controllers/augur_data_controller.rb
+++ app/controllers/augur_data_controller.rb
@@ -1,5 +1,6 @@
 class AugurDataController < ApplicationController
 	def create
+		p params.inspect
 		AugurData.create!(data: params[:augur_data], person_id: Person.where(twitter_screen_name: params[:handle]).first.id)
 	end
 end
