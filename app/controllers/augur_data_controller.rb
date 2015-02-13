738faa2e0c6294a76beeced123a01f3bc21518c0
Tim Hui <timboh56@gmail.com>
2015-02-12 23:10:35 -0800

    skip authenticity token

--- app/controllers/augur_data_controller.rb
+++ app/controllers/augur_data_controller.rb
@@ -1,6 +1,9 @@
 class AugurDataController < ApplicationController
+	skip_before_filter :verify_authenticity_token, only: [ :create ]
+
 	def create
 		p params.inspect
-		AugurData.create!(data: params[:augur_data], person_id: Person.where(twitter_screen_name: params[:handle]).first.id)
+		AugurData.create!(data: params, person_id: Person.where(twitter_screen_name: params[:PROFILES][:handle]).first.id)
+		render nothing: true
 	end
 end
