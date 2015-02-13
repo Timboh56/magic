6fb00fc8161056153260fd16e4ae21a4f05335fb
Tim Hui <timboh56@gmail.com>
2015-02-13 15:55:25 -0800

    check for status, only save 200 responses

--- app/controllers/augur_profiles_controller.rb
+++ app/controllers/augur_profiles_controller.rb
@@ -3,14 +3,18 @@ class AugurProfilesController < ApplicationController
 
 	def create
 		p params.inspect
-		if params[:PROFILES] && params[:PROFILES][:twitter_handle]
-			p params[:PROFILES][:twitter_handle].to_s
-			if params[:PROFILES][:twitter_handle].is_a? Array
-				AugurProfile.create!(data: params, person_id: Person.where(twitter_screen_name: params[:PROFILES][:twitter_handle][0][:value]).first.id) rescue nil
-			else
-				AugurProfile.create!(data: params, person_id: Person.where(twitter_screen_name: params[:PROFILES][:twitter_handle]).first.id) rescue nil
-			end
+		if params[:status] == 200
+			if params[:PROFILES] && params[:PROFILES][:twitter_handle]
+				p params[:PROFILES][:twitter_handle].to_s
+				if params[:PROFILES][:twitter_handle].is_a? Array
+					AugurProfile.create!(data: params, person_id: Person.where(twitter_screen_name: params[:PROFILES][:twitter_handle][0][:value]).first.id) rescue nil
+				else
+					AugurProfile.create!(data: params, person_id: Person.where(twitter_screen_name: params[:PROFILES][:twitter_handle]).first.id) rescue nil
+				end
 
+			end
+		else
+			p "Received a status of: " + params[:status].to_s
 		end
 		render nothing: true
 	end
